dc5cba2012e8bc318eab1e60194198dc22239e1c
Madraceee <raajanitheesh@gmail.com>
2023-03-06 21:35:23 +0530

    Added Git ignore Path

--- src/index.js
+++ src/index.js
@@ -9,4 +9,19 @@ function RemoveLogs() {
         cwd: process.cwd(),
         ignore: ["node_modules/**"],
     };
+    // Read the contents of the user's .gitignore file (if it exists)
+    const gitignorePath = `${process.cwd()}/.gitignore`;
+    if (fs.existsSync(gitignorePath)) {
+        const gitignoreData = fs.readFileSync(gitignorePath, 'utf8');
+        const gitignoreLines = gitignoreData.split('\n').map(line => line.trim()).filter(line => !!line && !line.startsWith('#'));
+        options.ignore.push(...gitignoreLines);
+    }
+    
+    // Read the contents of the user's .consoleLogIgnorer file (if it exists)
+    const consoleLogIgnorerPath = `${process.cwd()}/.logignore`;
+    if (fs.existsSync(consoleLogIgnorerPath)) {
+        const consoleLogIgnorerData = fs.readFileSync(consoleLogIgnorerPath, 'utf8');
+        const consoleLogIgnorerLines = consoleLogIgnorerData.split('\n').map(line => line.trim()).filter(line => !!line && !line.startsWith('#'));
+        options.ignore.push(...consoleLogIgnorerLines);
+    }
 }
